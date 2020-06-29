eb8ecfae28a00f1835952859686189ecc372a789
Gabriela Pazmiño <gnpazmin@espol.edu.ec>
2020-06-29 15:16:36 -0500

    Arreglo de botones

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,32 +1,39 @@
 
 public class Interfaz {
 		
-		private VBox panel= new VBox();
-		private HBOx p=new HBox();
-		private Button boton1= new Button("boton 1");
-		private Button boton2= new Button("boton 2");
-		private Button boton3= new Button("boton 3");
+		private VBox root= new VBox();
+		private JPanel panel=new JPanel();
+		private JButton boton1;
+		private JButton boton2;
+		private JButton boton3;
+		
+		
+		public Interfaz() {
+		boton1= new JButton("boton 1");
+		boton2= new JButton("boton 2");
+		boton3= new JButton("boton 3");
+		organizarPantalla();
+		cambioBotones();
+		}
+		
 		
 		public void organizarPantalla() {
-			
-		p.getChildren().add(boton1,boton2,boton3);
+		root.getChildren().add(p);
+		panel.getPnlFamilias().add(boton1,boton2,boton3);
+		
 		panel.getChildren().add(p);
 		boton1.setOnAction(e)
-		
 		}
 		
 		public void cambioBotones() {
-		
-			boton1.setOnAction(e->cambioDeColor);
-			boton2.setOnAction(e->cambioDeColor);
-			boton3.setOnAction(e->cambioDeColor);
-			
+			boton1.setOnAction(e->cambioDeColor("PURPLE"));
+			boton2.setOnAction(e->cambioDeColor("BLUE"));
+			boton3.setOnAction(e->cambioDeColor("BLACK"));
 		}
 
-		public void cambioDeColor() {
-			
-			
-			
+		
+		public void cambioDeColor(String color) {
+			panel.setBackground(Color.color);
 		}
 
 	
