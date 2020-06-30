56594072b735c91eb0da4444eb0af72cd1251c37
Gabriela Pazmiño <gnpazmin@espol.edu.ec>
2020-06-30 18:55:24 -0500

    Mejoras en interfaz

--- src/Ventana.java
+++ src/Ventana.java
@@ -18,17 +18,20 @@ public class Ventana extends JFrame {
 		public ArrayList<String> colores = new ArrayList<>();
 		private int clicks = 0;
 		private JPanel panel= new JPanel();
-		private JLabel et= new JLabel("Elige un color");
+		private JPanel panelResultado= new JPanel();
+		private JLabel et= new JLabel();
+		private JLabel et2= new JLabel();
 		private JButton btn1 = new JButton("Boton 1");
 		private JButton btn2 = new JButton("Boton 2");
 		private JButton btn3 = new JButton("Boton 3");
-		private JButton btn4 = new JButton("INFORME");
+		private JButton btn4 = new JButton("* INFORME *");
 		private Color col;
 		private String nombCol;
 		
+		
 		//CONSTRUCTOR
 		public Ventana() {
-			this.setSize(620, 500);
+			this.setSize(900, 500);
 			this.setLocationRelativeTo(null);
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 			this.setTitle("Cambio de colores de Botones");
@@ -43,30 +46,42 @@ public class Ventana extends JFrame {
 			colocarBotones();
 		}
 		
+		
 		public void organizarPanel() {
 			panel.setLayout(null);
 			this.getContentPane().add(panel);
 			panel.setBackground(Color.WHITE);
+			et.setText("    Elige un boton y selecciona el color que desees");
+			et.setOpaque(true);
+			et.setBackground(Color.white);
+			et.setBounds(150, 10, 300, 50);
 			panel.add(et);
+			et2.setText("     Cada 10 pulsaciones de botones de diferentes colores se podra presentar un informe por consola");
+			et2.setOpaque(true);
+			et2.setBackground(Color.white);
+			et2.setBounds(50, 50, 600, 50);
+			panel.add(et2);
 		}
+		
 		 
-
+		
 		
 		public void colocarBotones() {
 			btn1.setForeground(Color.white);
-			btn1.setBounds(100, 100, 100, 100);
+			btn1.setBounds(200, 200, 100, 100);
 			btn1.setBackground(Color.black);
 
 			btn2.setForeground(Color.white);
-			btn2.setBounds(200, 100, 100, 100);
+			btn2.setBounds(300, 200, 100, 100);
 			btn2.setBackground(Color.blue);
 
 			btn3.setForeground(Color.white);
-			btn3.setBounds(300, 100, 100, 100);
+			btn3.setBounds(400, 200, 100, 100);
 			btn3.setBackground(Color.LIGHT_GRAY);
 			
 			btn4.setForeground(Color.white);
-			btn4.setBounds(400, 100, 100, 100);
+			btn4.setBounds(600, 200, 200, 100);
+			btn4.setForeground(Color.black);
 			btn4.setBackground(Color.yellow);
 			
 			panel.add(btn1);
